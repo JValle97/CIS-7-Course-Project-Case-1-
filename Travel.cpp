f03f3c469b387bd7d8e7bfdbb31b147f65a7a1c6
JValle97 <111409262+JValle97@users.noreply.github.com>
2023-05-07 21:01:02 -0700

    Changes to travel.cpp
    Added code to adjacency and matrix function. When functions are called they now display correct info. Test messages are still present.

--- Travel.cpp
+++ Travel.cpp
@@ -1,5 +1,6 @@
 #include "Travel.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 
@@ -26,21 +27,75 @@ void Travel::shortestpath()
 /******************************************************************************/
 void Travel::adjacency()
 {
+    matrix();
     cout << endl << "adjacenccy function working" << endl;
+    
+    cout << temp[start - 1].city << endl;
+    for (int index = 0; index < 4; index++)
+    {
+        if (index == 0 || index == 2)
+        {
+            cout << setw(8) << "=> " << temp[index].city 
+                 << "\t" << temp[index].distance << " Mi"<< endl;
+        }
+        else
+            cout << setw(8) << "=> " << temp[index].city 
+                 << "\t\t" << temp[index].distance << " Mi"<< endl;
+    }
+    
+    cout << endl;
 }
 /******************************************************************************/
 /******************************************************************************/
 void Travel::matrix()
 {
-    Route temp[3];
     cout << endl << "matrix function working" << endl;
+    
+        temp[0].city = "Riverside";
+        temp[0].end = 1;
+        
+        temp[1].city = "Perris";
+        temp[1].end = 2;
+        
+        temp[2].city = "Moreno Valley";
+        temp[2].end = 3;
+        
+        temp[3].city = "Hemet";
+        temp[3].end = 4;
+    
     if (start == 1)
     {
-        temp[1].end = 2;
+        temp[0].distance = 0;
         temp[1].distance = 24;
+        temp[2].distance = 16;
+        temp[3].distance = 33;
     }
-    cout<<"("<<start<<", "<<temp[1].end<<", "<<temp[1].distance<<")\n";
-    //Route RivPer(1, 2, 24);
+    else if (start == 2)
+    {
+        temp[0].distance = 24;
+        temp[1].distance = 0;
+        temp[2].distance = 18;
+        temp[3].distance = 30;
+    }
+    else if (start == 3)
+    {
+        temp[0].distance = 16;
+        temp[1].distance = 18;
+        temp[2].distance = 0;
+        temp[3].distance = 26;
+    }
+    else if (start == 4)
+    {
+        temp[0].distance = 33;
+        temp[1].distance = 30;
+        temp[2].distance = 26;
+        temp[3].distance = 0;
+    }
+    if (selection == 'D')
+    {
+        for (int i = 0; i < 4; i++)
+            cout<<"("<<start<<", "<<temp[i].end<<", "<<temp[i].distance<<")\n";
+    }
 }
 /******************************************************************************/
 /******************************************************************************/
